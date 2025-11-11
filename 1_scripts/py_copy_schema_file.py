9128ee2e1f54ef072431244631f7dce50490910e
thangckt <thangckt@gmail.com>
2025-11-11 13:42:51 +0000

    Update from https://github.com/thangckt/alff/commit/1c7ec64bf1e0414f7c88f27671d50a8fd5db15c4

---
+++ 1_scripts/py_copy_schema_file.py
@@ -0,0 +1,138 @@
+from thkit import THKIT_ROOT
+
+from alff.util.key import (
+    SCHEMA_ACTIVE_LEARN,
+    SCHEMA_ASE_BUILD,
+    SCHEMA_ASE_RUN,
+    SCHEMA_ELASTIC,
+    SCHEMA_FINETUNE,
+    SCHEMA_GENDATA,
+    SCHEMA_LAMMPS,
+    SCHEMA_PES_SCAN,
+    SCHEMA_PHONON,
+)
+
+EXAMPLE_PATH = "example"  # relative path to project_dir
+
+
+def append_schema_text(md_file: str, yaml_schema_file: str):
+    """Append yaml text to .md file."""
+    with open(yaml_schema_file, "r") as f:
+        text = f.read()
+    schema_text = "\n```yaml\n" + text + "\n```\n"
+    schema_text = "\n## Schema:\n" + schema_text
+    ### append yaml schema
+    with open(md_file, "a") as f:
+        f.write(schema_text)
+    return
+
+
+def append_example_config(md_file: str, config_files: list[str]):
+    """Append yaml text to .md file."""
+    for i, yaml_file in enumerate(config_files):
+        with open(yaml_file, "r") as f:
+            text = f.read()
+        schema_text = "\n```yaml\n" + text + "\n```\n"
+        schema_text = f"\n## Example config {i + 1}:\n" + schema_text
+        ### append yaml schema
+        with open(md_file, "a") as f:
+            f.write(schema_text)
+    return
+
+
+def main():
+    ###ANCHOR: Append schema to the .md files
+    append_schema_text(
+        md_file="./_docs/schema/manual_schema_gendata.md",
+        yaml_schema_file=SCHEMA_GENDATA,
+    )
+    append_schema_text(
+        md_file="./_docs/schema/manual_schema_active_learning.md",
+        yaml_schema_file=SCHEMA_ACTIVE_LEARN,
+    )
+    append_schema_text(
+        md_file="./_docs/schema/manual_schema_finetune.md",
+        yaml_schema_file=SCHEMA_FINETUNE,
+    )
+    append_schema_text(
+        md_file="./_docs/schema/manual_schema_phonon.md",
+        yaml_schema_file=SCHEMA_PHONON,
+    )
+    append_schema_text(
+        md_file="./_docs/schema/manual_schema_elastic.md",
+        yaml_schema_file=SCHEMA_ELASTIC,
+    )
+    append_schema_text(
+        md_file="./_docs/schema/manual_schema_pes_scan.md",
+        yaml_schema_file=SCHEMA_PES_SCAN,
+    )
+    append_schema_text(
+        md_file="./_docs/schema/manual_schema_ase_build.md",
+        yaml_schema_file=SCHEMA_ASE_BUILD,
+    )
+    append_schema_text(
+        md_file="./_docs/schema/manual_schema_ase_run.md",
+        yaml_schema_file=SCHEMA_ASE_RUN,
+    )
+    append_schema_text(
+        md_file="./_docs/schema/manual_schema_lammps.md",
+        yaml_schema_file=SCHEMA_LAMMPS,
+    )
+
+    ###ANCHOR: Append example configuration to the .md files
+    append_example_config(
+        md_file="./_docs/schema/manual_schema_gendata.md",
+        config_files=[f"{EXAMPLE_PATH}/gdata/sampleConfig_gendata.yml"],
+    )
+    append_example_config(
+        md_file="./_docs/schema/manual_schema_active_learning.md",
+        config_files=[f"{EXAMPLE_PATH}/al/sampleConfig_active_learn.yml"],
+    )
+    append_example_config(
+        md_file="./_docs/schema/manual_schema_finetune.md",
+        config_files=[f"{EXAMPLE_PATH}/al/sampleConfig_finetune.yml"],
+    )
+
+    append_example_config(
+        md_file="./_docs/schema/manual_schema_phonon.md",
+        config_files=[
+            f"{EXAMPLE_PATH}/phonon/sampleConfig_phonon_gpaw.yml",
+            f"{EXAMPLE_PATH}/phonon/sampleConfig_phonon_lammps.yml",
+        ],
+    )
+
+    append_example_config(
+        md_file="./_docs/schema/manual_schema_pes_scan.md",
+        config_files=[
+            f"{EXAMPLE_PATH}/pes/sampleConfig_pes_scan_gpaw.yml",
+            f"{EXAMPLE_PATH}/pes/sampleConfig_pes_scan_lammps.yml",
+        ],
+    )
+
+    append_example_config(
+        md_file="./_docs/schema/manual_schema_ase_run.md",
+        config_files=[f"{EXAMPLE_PATH}/script/sampleConfig_ase_run.yml"],
+    )
+    append_example_config(
+        md_file="./_docs/schema/manual_schema_ase_build.md",
+        config_files=[f"{EXAMPLE_PATH}/script/sampleConfig_ase_build.yml"],
+    )
+    append_example_config(
+        md_file="./_docs/schema/manual_schema_lammps.md",
+        config_files=[f"{EXAMPLE_PATH}/script/sampleConfig_lammps.yml"],
+    )
+
+    ###ANCHOR: Schema from thkit package
+    append_schema_text(
+        md_file="./_docs/schema/manual_schema_machine.md",
+        yaml_schema_file=f"{THKIT_ROOT}/jobman/schema/schema_machine.yml",
+    )
+    append_example_config(
+        config_files=[f"{THKIT_ROOT}/jobman/schema/sampleConfig_machine_single.yml"],
+        md_file="./_docs/schema/manual_schema_machine.md",
+    )
+    return
+
+
+if __name__ == "__main__":
+    main()
